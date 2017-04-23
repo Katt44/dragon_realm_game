fadafaabc0e4000da9a38e97450e739520a9faa5
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-22 19:43:55 -0700

    added pseudo code comments and basic method set up

---
+++ dragon_realm_game.py
@@ -0,0 +1,15 @@
+import random 
+import time
+
+# having the user pick between to caves, that randomly are friendly or dangerous
+# printing text out slowly for suspense in  checking the cave
+# asking user if they want to play again
+
+def display_intro():
+
+
+def choose_a_cave():
+
+def check_cave():
+
+def play_again():

d51aba7142d5adf3be64d8254897b149cc06edd9
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-30 13:56:46 -0700

    added to choose a cave function

--- dragon_realm_myversion.py
+++ dragon_realm_myversion.py
@@ -7,6 +7,10 @@ and you exhale as you pass through the threshold.
 The cave up ahead  forks but both paths are damp, seeming to breathing, and onimnious. ''')
 
 def choose_a_cave():
+	cave = raw_input("What cave will you enter? left or right?")
+	cave = cave.lower()
+	print "you chose to go " + cave + " good luck."
+	return cave 
 	
 
 def check_cave(chosen_cave):
@@ -15,3 +19,7 @@ def check_cave(chosen_cave):
 
 
 #call functions
+def play_dragon_realm()
+	display_intro()
+	choose_a_cave()
+	check_cave()

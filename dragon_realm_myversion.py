2dedeffdd4e89db5b23b35c8e28f9cc6b546456b
Hack bright Unicorn <Brighticorn@Cuvette.local>
2017-04-30 13:51:15 -0700

    set up empty func

---
+++ dragon_realm_myversion.py
@@ -0,0 +1,17 @@
+import random 
+import time
+
+def display_intro():
+	print ('''Standing at the mouth of a cave you take one long inhale 
+and you exhale as you pass through the threshold.
+The cave up ahead  forks but both paths are damp, seeming to breathing, and onimnious. ''')
+
+def choose_a_cave():
+	
+
+def check_cave(chosen_cave):
+
+
+
+
+#call functions
